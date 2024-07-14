c52bfad239059b145fa1b5e6c7799ce66f29c216
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2024-07-14 16:27:00 -0700

    meal detail, error , not-found pages added

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,13 +1,38 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React, { Suspense } from "react";
+import classes from "./page.module.css";
+import MealGrid from "@/components/meals/meal-grid";
+import { getMeals } from "@/lib/meals";
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealGrid meals={meals} />;
+}
 
 const MealPage = () => {
   return (
-    <h1>
-      meals 
-      <Link href='/meals/product-1'>Product 1</Link>
-    </h1>
-  )
-}
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your Favourite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching the Meals....</p>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
+};
 
-export default MealPage
+export default MealPage;
---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+
+export default function NotFound() {
+  return (
+    <main className='not-found'>
+      <h1>Not Found</h1>
+      <p>Unfortunately! we could find the request page or resource</p>
+    </main>
+  )
+}
