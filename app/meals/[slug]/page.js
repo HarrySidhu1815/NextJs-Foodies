5a67303b2faf11bf5867d41f57150283b198f525
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2024-07-20 09:05:18 -0700

    complete project with aws storage configuration, metadata, and share recipes

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -3,19 +3,34 @@ import classes from './page.module.css'
 import {getMeal} from '@/lib/meals'
 import { notFound } from 'next/navigation'
 
+
+export async function generateMetadata({params}){
+  const meal = getMeal(params.slug)
+
+  if(!meal){
+    notFound()
+  }
+  
+  return{
+    title: meal.title,
+    description: meal.summary
+  }
+}
+
 const MealDetailPage = ({params}) => {
   const meal = getMeal(params.slug)
 
   if(!meal){
     notFound()
   }
+  
 
   meal.instructions = meal.instructions.replace(/\n/g, '<br />')
   return (
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image src={meal.image} alt={meal.title} fill/>
+          <Image src={`https://harjobanpreet-nextjs-demo-user-image.s3.us-east-2.amazonaws.com/${meal.image}`} alt={meal.title} fill/>
         </div>
         <div className={classes.headerText}>
           <h1>{meal.title}</h1>
--- app/meals/page.js
+++ app/meals/page.js
@@ -4,6 +4,12 @@ import classes from "./page.module.css";
 import MealGrid from "@/components/meals/meal-grid";
 import { getMeals } from "@/lib/meals";
 
+export const metadata = {
+    title: 'All Meals',
+    description: 'Browse the delicious meals shared by our vibrant community.',
+  };
+
+  
 async function Meals() {
   const meals = await getMeals();
   return <MealGrid meals={meals} />;
